94e29940792db0d98e8779c7eeaa5cc5bb9b35bb
NAFONYA <55001188+NAFONYA@users.noreply.github.com>
2019-09-24 17:02:49 +0300

    Add files via upload

---
+++ lab1/js/search.js
@@ -0,0 +1,12 @@
+// handling the Enter key
+document.querySelector('#search').addEventListener('keydown', function(e) {
+  if (e.keyCode === 13) {
+    startSearch();
+  }
+});
+
+// start search function
+
+
+// TODO:
+// - make highlithghtghting for search result
